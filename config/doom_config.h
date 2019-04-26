ea8f1eceaaafce4d3395a62a96a9a9f500f80df5
Audun Wilhelmsen <skyfex@gmail.com>
2019-04-26 14:32:40 +0200

    Copy initial files from Chocolate Doom

---
+++ config/doom_config.h
@@ -0,0 +1,32 @@
+
+/* Name of package */
+#define PACKAGE "chocolate-doom"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "audun.wilhelmsen@nordicsemi.no"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "NRF Doom"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "NRF Doom 0.1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "nrf-doom"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1.0"
+
+/* Change this when you create your awesome forked version */
+#define PROGRAM_PREFIX "nrf-"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.1.0"
+
+/* Define to 1 if your processor stores words with the most significant byte
+   first (like Motorola and SPARC, unlike Intel and VAX). */
+/* #undef WORDS_BIGENDIAN */
+
