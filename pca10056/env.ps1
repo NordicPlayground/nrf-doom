41ff3c71ce6466f8e795a50b9d2d8f6c617800b4
auwi <audun.wilhelmsen@nordicsemi.no>
2021-03-29 13:44:53 +0200

    Remove old build directory for nrf52

--- pca10056/env.ps1
+++
@@ -1,3 +0,0 @@
-$env:Path += ";C:\SW\MinGW\msys\1.0\bin"
-$env:Path += ";C:\Program Files (x86)\GNU Tools ARM Embedded\6 2017-q1-update\bin"
-$env:Path += ";C:\Program Files (x86)\Nordic Semiconductor\nrf-command-line-tools\bin"
